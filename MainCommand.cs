f565cbeb3950d3d18e7249bc9497b480c0a9573f
AskoldMakaruk <askoldmakaruk@gmail.com>
2019-12-02 17:46:33 +0200

    migration from System.Drawing to ImageSharp lib

--- MainCommand.cs
+++ MainCommand.cs
@@ -1,19 +1,38 @@
 ﻿using System;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
-using System.Drawing.Text;
 using System.IO;
 using System.Linq;
 using BotFramework;
 using BotFramework.Bot;
 using BotFramework.Commands;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.Primitives;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.InputFiles;
 namespace WhoTheFuckBot.Telegram.Commands
 {
     public class MainCommand : IStaticCommand
     {
+        private static Image<Rgba32> _template;
+        public static Image<Rgba32> Template
+        {
+            get
+            {
+                if (_template == null)
+                {
+                    var bytes = typeof(MainCommand).Assembly.GetManifestResourceStream(Resources.First(c => c.Contains(".png")));
+                    _template = Image.Load(bytes) as Image<Rgba32>;
+                }
+                return _template;
+            }
+        }
+
+        private static readonly string[] Resources = typeof(MainCommand).Assembly.GetManifestResourceNames();
+        private static readonly FontCollection Fonts = new FontCollection();
+        private static readonly FontFamily Arial = Fonts.Install(typeof(MainCommand).Assembly.GetManifestResourceStream(Resources.First(c => c.Contains(".ttf"))));
+
         public Response Execute(Message message, Client client)
         {
             // using var controller = new Controllers.Controller();
@@ -23,73 +42,32 @@ namespace WhoTheFuckBot.Telegram.Commands
 
             try
             {
-                var resources = GetType().Assembly.GetManifestResourceNames();
-
-                Bitmap bmp = new Bitmap(this.GetType().Assembly.GetManifestResourceStream(resources.First(c => c.Contains(".png"))));
-
-                var rect = new Rectangle(160, 85, 130, 80);
-
-                Graphics g = Graphics.FromImage(bmp);
-
-                g.FillRectangle(Brushes.White, new Rectangle(160, 85, 130, 70));
-                g.SmoothingMode = SmoothingMode.AntiAlias;
-                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-                var fontStream = this.GetType().Assembly.GetManifestResourceStream(resources.First(c => c.Contains(".ttf")));
-                var pfc = new PrivateFontCollection();
-
-                byte[] fontdata = new byte[fontStream.Length];
-                fontStream.Read(fontdata, 0, (int) fontStream.Length);
-                fontStream.Close();
-                unsafe
+                using var image = Template.Clone();
+                var rectangle = new []
                 {
-                    fixed(byte * pFontData = fontdata)
-                    {
-                        pfc.AddMemoryFont((System.IntPtr) pFontData, fontdata.Length);
-                    }
-                }
+                    new PointF(160, 85),
+                    new PointF(160 + 130, 85),
+                    new PointF(160 + 130, 85 + 80),
+                    new PointF(160, 85 + 80),
+                };
+                image.Mutate(cl => cl.FillPolygon(GraphicsOptions.Default, Brushes.Solid(Color.White), rectangle));
 
-                var font = new Font(pfc.Families[0], 16);
+                var font = Arial.CreateFont(16, FontStyle.Regular);
+                var size = TextMeasurer.Measure(message.Text, new RendererOptions(font));
 
-                font = GetAdjustedFont(g, message.Text, font, rect.Width, 50, 12, true);
+                float scalingFactor = 80 / size.Height;
+                var scaledFont = new Font(font, scalingFactor * font.Size);
 
-                g.DrawString(message.Text, font, Brushes.Black, rect);
-
-                g.Flush();
+                image.Mutate(cl => cl.DrawText(message.Text, scaledFont, Color.Black, new PointF(170, 90)));
                 var str = new MemoryStream();
-
-                bmp.Save(str, ImageFormat.Png);
+                image.SaveAsJpeg(str);
                 str.Seek(0, SeekOrigin.Begin);
-
                 return new Response(this, new SetBitmapPath()).SendPhoto(message.From.Id, new InputOnlineFile(str, "photo.png"));
             }
             catch (Exception e)
             {
                 client.Write(e.ToString());
                 return new Response(this, new SetBitmapPath()).SendTextMessage(message.From.Id, "504 internal server error.");
-            }
-        }
-
-        public Font GetAdjustedFont(Graphics g, string graphicString, Font originalFont, int containerWidth, int maxFontSize, int minFontSize, bool smallestOnFail)
-        {
-            Font testFont = null;
-            for (int adjustedSize = maxFontSize; adjustedSize >= minFontSize; adjustedSize--)
-            {
-                testFont = new Font(originalFont.Name, adjustedSize, originalFont.Style);
-                SizeF adjustedSizeNew = g.MeasureString(graphicString, testFont);
-                if (containerWidth > Convert.ToInt32(adjustedSizeNew.Width))
-                {
-                    return testFont;
-                }
-            }
-            if (smallestOnFail)
-            {
-                return testFont;
-            }
-            else
-            {
-                return originalFont;
             }
         }
 
